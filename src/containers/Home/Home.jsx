ca1b0dec78abf72ce4d1d40b4c98086a31254ea3
Chad Ostrowski <hi@chadoh.com>
2016-01-23 12:50:56 -0500

    Rearrange home page

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -64,6 +64,12 @@ class Details extends React.Component {
           <div className="container">
             <p style={{marginBottom: 0}}>Just as reflecting on the past can give us a sense of gratitude, reflecting on the future can give us a sense of urgency. </p>
           </div>
+
+          <p className="centered">
+            <Link to="/signin" className="button">
+              Claim my free life calendar now
+            </Link>
+          </p>
         </div>
       </div>
     );
@@ -100,6 +106,23 @@ export default class Home extends Component {
             </Link>
           </p>
         </Landing>
+
+        <PageSection type="sunset-blocked" className="container">
+          <div>
+            <h2 className="brand">The Forest &amp; The Branches</h2>
+            <p>It's easy to get stuck in the monotonous slog of the present.</p>
+            <p>It's easy to lose awareness, and to let each week slip by without thought.</p>
+            <p>It's easy to stop being intentional about how we live our lives.</p>
+            <p>Most likely, all the other websites and apps that you use today will only increase your tendency to lose context. They'll keep you focused on a view of the branches right around you.</p>
+            <p>Entire.Life will lift you above it all, and show you a view of the whole forest. The whole, beautiful forest of your life.</p>
+            <div className="centered">
+              <Link to="/signin" className="button">
+                Claim my free life calendar now
+              </Link>
+            </div>
+          </div>
+        </PageSection>
+
         <PageSection type="light">
           <div className="container">
             <div className="row verticallyCentered">
@@ -146,8 +169,14 @@ export default class Home extends Component {
                 </p>
               </div>
             </div>
+            <div className="centered" style={{marginTop: '1em'}}>
+              <Link to="/signin" className="button">
+                Sign up for a tour
+              </Link>
+            </div>
           </div>
         </PageSection>
+
         <PageSection type="dark">
           <div className="rounded-images">
             <div className="container">
@@ -164,61 +193,6 @@ export default class Home extends Component {
             </div>
 
             <Details/>
-          </div>
-        </PageSection>
-
-        <PageSection type="sunset-blocked" className="container">
-          <div>
-            <h2 className="brand">The Forest &amp; The Branches</h2>
-            <p>It's easy to get stuck in the monotonous slog of the present.</p>
-            <p>It's easy to lose awareness, and to let each week slip by without thought.</p>
-            <p>It's easy to stop being intentional about how we live our lives.</p>
-            <p>Most likely, all the other websites and apps that you use today will only increase your tendency to lose context. They'll keep you focused on a view of the branches right around you.</p>
-            <p>Entire.Life will lift you above it all, and show you a view of the whole forest. The whole, beautiful forest of your life.</p>
-            <div className="centered">
-              <Link to="/signin" className="button">
-                Claim my free life calendar now
-              </Link>
-            </div>
-          </div>
-        </PageSection>
-
-        <PageSection type="dark">
-          <div className="container">
-            <h2 className="brand">Hall of Fame</h2>
-            <p>People we celebrate. Click through to see their life calendars.</p>
-          </div>
-          <div className="containerWide smaller">
-            <div className="row">
-              <div className="col6">
-                <div className="row verticallyCentered">
-                  <div className="col4">
-                    <Link to="/bessie-coleman">
-                      <img className="circle" src={require('./bessie-coleman.jpg')} alt="photo of Bessie Coleman as a young woman in aviator garb"/>
-                    </Link>
-                  </div>
-                  <div className="col8">
-                    <h3 className="brand">Bessie Coleman</h3>
-                    <p>Black daredevil aviatrix. When no one in racist America would teach her to fly, this badass went to flight school in France. Never got to start her own flight school.</p>
-                    <Link to="/bessie-coleman" className="button">View her Entire.Life</Link>
-                  </div>
-                </div>
-              </div>
-              <div className="col6">
-                <div className="row verticallyCentered">
-                  <div className="col4">
-                    <Link to="/bayard-rustin">
-                      <img className="circle" src={require('./bayard-rustin.jpg')} alt="photo of Bessie Coleman as a young woman in aviator garb"/>
-                    </Link>
-                  </div>
-                  <div className="col8">
-                    <h3 className="brand">Bayard Rustin</h3>
-                    <p>A voice behind many of the voices of the American Civil Rights movement. Organizer of the 1963 March on Washington for Jobs and Freedom. American hero.</p>
-                    <Link to="/bayard-rustin" className="button">View his Entire.Life</Link>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </PageSection>
         <Footer/>
